1c7e8d52e89b75758d6effc7b23ad4f816c98f0e
vitalydok <89067055+vitalydok@users.noreply.github.com>
2022-01-01 20:00:59 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
 import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import App from "./app";
+import App from "./app/App";
+
 ReactDOM.render(
-  <>
-    <App />
-  </>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
